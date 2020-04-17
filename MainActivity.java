131e04ec066252b72cd1808a851e4f3c9ee12b5e
awojty <55328874+awojty@users.noreply.github.com>
2020-04-17 17:51:15 -0400

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.myapplication_udemy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+
+
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ * TextView textVieww=new TextView(this) context is the same so this;
+ */
+public class MainActivity extends AppCompatActivity {
+    public int numberSatisfaction=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitResponse(View view) {
+        EditText name=(EditText) findViewById(R.id.namefield);
+        String value=name.getText().toString();
+        CheckBox froshCheckBox = (CheckBox) findViewById(R.id.frosh_checkbox);
+        CheckBox secondCheckBox = (CheckBox) findViewById(R.id.secon_year_checkbox);
+        CheckBox juniorCheckBox = (CheckBox) findViewById(R.id.junior_checkbox);
+        CheckBox seniorCheckBox = (CheckBox) findViewById(R.id.senior_checkbox);
+        boolean isSenior=seniorCheckBox.isChecked();
+        boolean isFrosh=froshCheckBox.isChecked();
+        boolean isJunior=juniorCheckBox.isChecked();
+        boolean isSecond=secondCheckBox.isChecked();
+        EditText comment=(EditText) findViewById(R.id.comment);
+        String comm=comment.getText().toString();
+        displayQuantity(numberSatisfaction);
+        displayMessage(createMessage(value, isFrosh, isSenior, isSecond, isJunior,comm));
+
+    }
+
+    private String createMessage(String name, boolean frosh, boolean second, boolean junior, boolean senior, String comment){
+        String year="student unspecified";
+        if(frosh==true){
+            year="Frosh";}
+        if(second==true){year="Second";}
+        if(junior==true){year="Junior";}
+        if(senior==true){year="Senior";}
+        String message=
+                "\n"+"Name"+name +
+                "\n"+"Year "+ year+
+//                "\n"+getString(R.string.order_summary_chocolate, choc)+
+                "\n"+"Total: "+Integer.toString(numberSatisfaction) +
+                        "\n"+"Comm: "+comment +
+                "\n"+getString(R.string.thank_you);
+        return message;
+    }
+
+    public void increment(View view) {
+        numberSatisfaction++;
+        displayQuantity(numberSatisfaction);
+
+    }
+
+    public void decrement(View view) {
+        if (numberSatisfaction>0){numberSatisfaction--;}
+        displayQuantity(numberSatisfaction);
+
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     *first find a etxt to be change by find by id, hthen set a text with a passed arguemnt in set text methid callde with the obect
+     */
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+
+    private void displayMessage(String message) {
+        TextView SummaryTextView = (TextView) findViewById(R.id.summary_text_view);
+        SummaryTextView.setText(message);
+    }
+
+
+
+
+}
